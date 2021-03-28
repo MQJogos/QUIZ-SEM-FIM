238b8e5947369bdfa88490965f87c4ce7b3d2bf5
Débora Inocêncio <30512134+TrechosDeCodigos@users.noreply.github.com>
2021-03-27 21:49:15 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
-function Quiz(questions){
+function Quiz(questions)
+{
 this.score=0;
 this.questions=questions;
 this.questionIndex=0;
 }
 
-Quiz.prototype.getQuestionIndex = function(){
+Quiz.prototype.getQuestionIndex = function()
+{
 return this.questions[this.questionIndex];
 }
 
-Quiz.prototype.guess = function(answer){
-if(this.getQuestionIndex().iscorrectAnswer(answer))
+Quiz.prototype.guess = function(answer)
+{
+if(this.getQuestionIndex().iscorrectAnswer(answer)){
+this.score++;
+}
+this.questionIndex++;
+}
+
+Quiz.prototype.isEnded = function()
+{
+  return this.questionIndex ===
+  this.questions.length;
 }
