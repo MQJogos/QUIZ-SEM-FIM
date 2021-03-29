5bd5912124ae121cefd0c98943a4a84b54912f1a
Débora Inocêncio <30512134+TrechosDeCodigos@users.noreply.github.com>
2021-03-29 19:28:38 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,6 +6,39 @@ function populate()
 else{
 var element = document.getElementById("question");
 element.innerHTML = quiz.getQuestionIndex().text;
+
 var choices = quizQuestionIndex().choices;
 for(var i=0; i< choices.length; i++{
-var element = document.getElementById
+var element = document.getElementById("choice" + i);
+element.innerHTML = choices[i];
+guess("bt"+i,choices[i]);
+}
+showProgress();
+}
+};
+
+function guess(id, guess)
+{
+  var button = document.getElementById(id);
+  button.onclick = function(){
+  quiz.guess(guess);
+  populate();
+}
+};
+
+ function showProgress(){
+ var currentQuestionNumber = quiz.questionIndex + 1;
+ var element = document.getElementByID("progress");
+ element.innerHTML = "Question" + currentQuestionNumber + "of" + quiz.questions.length;
+};
+
+function showscores(){
+var gameoverHTML = "<h1> RESULTADO </h1>;
+gameoverHTML += "<h2 id='score'> SUA PONTUAÇÃO: " + quiz.score + "</h2>";
+var element = document.getElementById("quiz");
+element.innerHTML = gameoverHTML;
+};
+
+//create questions 
+var questions = []
+new Question("")
