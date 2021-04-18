f1a5ae6e9c5a4962dcd11695f2f04b6ad5dba02b
Débora Inocêncio <30512134+TrechosDeCodigos@users.noreply.github.com>
2021-04-17 23:03:58 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,58 +1,68 @@
 function populate() {
-    if (quiz.isEnded() ) {
+    if(quiz.isEnded()) {
         showScores();
     }
-    else{
+    else {
+        // show question
         var element = document.getElementById("question");
         element.innerHTML = quiz.getQuestionIndex().text;
 
+        // show options
         var choices = quiz.getQuestionIndex().choices;
-        for (var i = 0; i < choices.length; i++) {
+        for(var i = 0; i < choices.length; i++) {
             var element = document.getElementById("choice" + i);
             element.innerHTML = choices[i];
             guess("bt" + i, choices[i]);
-        
         }
 
         showProgress();
     }
-    
 };
 
 function guess(id, guess) {
     var button = document.getElementById(id);
-    button.onclick = function(){
+    button.onclick = function() {
         quiz.guess(guess);
         populate();
     }
 };
 
 
-function showProgress(){
+function showProgress() {
     var currentQuestionNumber = quiz.questionIndex + 1;
     var element = document.getElementById("progress");
-    element.innerHTML = " PERGUNTA " + currentQuestionNumber + " DE " + quiz.questions.length;
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
 };
 
-function showScores(){
-    var gameOverHTML = "<h1> Resultado </h1>";
-    gameOverHTML += "<h2 id='score'> SUA PONTUAÇÃO: " + quiz.score + "</h2>";
-    var element = document.getElementByID("quiz");
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
     element.innerHTML = gameOverHTML;
 };
 
-
-//create questions
+// create questions
 var questions = [
-    new Question("Qual a capital da India?",["New Delhi","Mumbai","Jaipur","Kolkata"],"New Delhi"),
+   '.jpg','.jpg','.jpg' ,'ILHA.jpg'
+    new Question("_BACAXI", ["A", "I","E", "I"], "A"),
 
-    new Question("Animal Nacional da Índia?", ["Elephant", "Bengal Tiger","Lion", "Cow"], "Bengal Tiger"),
+    new Question("_SQUILO", ["A", "I","O", "E"], "E"),
     
-    new Question("Rio Nacional da Índia?", ["Yamuna", "Ganga","Brahmputra", "Sindhu"], "Ganga"),
+    new Question("_BELHA?", ["E", "I","A", "O"], "A"),
     
-    new Question("Moeda da Índia?", ["Dollar", "Rupee","Euro", "Pound"], "Rupee"),
+'ONIBUS.jpg','AVIAO.jpg','UNHAS.jpg','ELEFANTE
+    new Question("_NIBUS?", ["U", "E","I", "Ô"], "Ô"),
+        
+
 ];
 
-var quiz = new  Quiz(questions);
+
+var quiz = new Quiz(questions);
+
 
 populate();
+
+
+
+
+
